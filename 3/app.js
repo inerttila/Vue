3f43d30313a923dfcc0c125e6d5b524e41119b8d
inerttila <inert.etila@gmail.com>
2024-05-03 15:08:02 +0200

    add

--- 3/app.js
+++ 3/app.js
@@ -3,10 +3,33 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastname: "",
     };
   },
+
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+  },
+
+  computed: {
+    fullname() {
+      console.log("running again ...");
+      if (this.name === "" || this.lastname === "") {
+        return "";
+      }
+      return this.name + " " + this.lastname;
+    },
+  },
+
   methods: {
-    setName(event, lastName) {
+    setName(event) {
       this.name = event.target.value;
     },
     add(num) {
