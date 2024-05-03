e774bda21d0d47a929d5c727f3a67423b33da502
inerttila <inert.etila@gmail.com>
2024-05-03 11:53:54 +0200

    add`lastName;lastName;lastName;

--- 2/app.js
+++ 2/app.js
@@ -2,8 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+
+  methods: {
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    remove(num) {
+      this.counter = this.counter - num;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
