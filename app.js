414513bf6a9b1e2b1eda01e8f780015b69759591
inerttila <inert.etila@gmail.com>
2024-05-03 10:16:24 +0200

    ediy

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const app = Vue.createApp({
-    data () {
-        return {
-            courseGoalA: 'finish the course and learn  vue',
-            courseGoalB: 'this is the course goal',
-            vueLink:  'https://chat.openai.com/'
-        };
+  data() {
+    return {
+      courseGoalA: "finish the course and learn  vue",
+      courseGoalB: "this is the course goal",
+      vueLink: "https://chat.openai.com/",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const reendomNumber = Math.random();
+      if (reendomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
     },
-    methods :{
-        outputGoal () {
-            const  reendomNumber = Math.random();
-            if ( reendomNumber < 0.5 ) {
-                return this.courseGoalA;
-                } else {
-                return this.courseGoalB;
-                }
-        }
-    }
+  },
 });
 
-app.mount('#user-goal');   
+app.mount("#user-goal");
